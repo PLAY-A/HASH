8bb70d1a93063a9291c963f8ddfc69504c4217b4
PLAY <145706770+PLAY-A@users.noreply.github.com>
2023-10-07 12:41:39 -0700

    Add files via upload

---
+++ runner.py
@@ -0,0 +1,14 @@
+import DeepScanning
+
+# DeepScanning.virusRemover("/*folder*/")
+
+
+# DeepScanning.virusScanner("C:\Program Files")
+# print(DeepScanning.virusPath)
+
+DeepScanning.justFileRemover()
+
+
+
+
+
